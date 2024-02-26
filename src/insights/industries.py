1005029c8c73996ee38497e32b73808f50b25572
Felipe Duarte <feduarte.dev@gmail.com>
2024-02-26 12:35:04 -0300

    quinto requisito refatorado

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,14 +7,10 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        unique_industries = []
-        for row in self.jobs_list:
-            if (
-                row["industry"] not in unique_industries
-                and row["industry"] != ""
-            ):
-                unique_industries.append(row["industry"])
-        return unique_industries
+        unique_industries = set(
+            row["industry"] for row in self.jobs_list if row["industry"] != ""
+        )
+        return list(unique_industries)
 
 
 # teste = ProcessIndustries()
